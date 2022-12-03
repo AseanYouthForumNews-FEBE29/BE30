3c09670a4b90adb0aeb54cb57411ce4492ca1bbb
irfannudin ihsan <irfannudinihsan56@gmail.com>
2022-12-03 18:53:35 +0700

    return auth in user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -9,13 +9,13 @@ const imgbbUploader = require('imgbb-uploader')
 
 module.exports = {
     getAllUser: async(req, res) => {
-        // const auth = await req.headers.authorization
-        // const token = await auth.split(" ")[1]
+        const auth = await req.headers.authorization
+        const token = await auth.split(" ")[1]
 
-        // const verified = jwt.verify(token, process.env.JWTKEY)
+        const verified = jwt.verify(token, process.env.JWTKEY)
 
-        // if (verified){
-        //     if (verified.id == 1){
+        if (verified){
+            if (verified.id == 1){
                 const users = await UserDetail.findAll({
                     include: [{
                         model: User,
@@ -39,12 +39,12 @@ module.exports = {
                     res.sttus(204).json({message: "No Users Found"})
                 }
         
-        //     } else {
-        //         res.status(401).json({message: "Admin Only"})
-        //     }
-        // } else {
-        //     res.status(401).json({message: "Unauthorized"})
-        // }
+            } else {
+                res.status(401).json({message: "Admin Only"})
+            }
+        } else {
+            res.status(401).json({message: "Unauthorized"})
+        }
     },
 
     updateUserById: async(req, res) => {
