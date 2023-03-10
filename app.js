68be8cfa7c09ad4eee4593befdae42af08e9a006
irfannudin ihsan <irfannudinihsan56@gmail.com>
2023-03-10 09:38:27 +0700

    change CORS

--- app.js
+++ app.js
@@ -10,24 +10,14 @@ const PORT = process.env.PORT;
 
 const router = require("./routes");
 
-const corsConfig = {
-  credentials: true,
-  origin: "*",
-};
+
 
 app.use(express.json());
 
-app.use(cors(corsConfig));
-app.options("*", cors(corsConfig));
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  });
 
 app.use(router);
 
