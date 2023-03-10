b1e4e8f9d3d1541b16a2138dab38b27d65306dc5
irfannudin ihsan <irfannudinihsan56@gmail.com>
2023-03-10 09:47:10 +0700

    change CORS2

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(cors(corsConfig))
 app.options("*", cors(corsConfig))
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Credentials", "true")
+    res.header('Access-Control-Allow-Origin', '*')
     next()
 })
 
