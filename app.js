68aca7c13b73ec8da403082cebc13a007c9568da
irfannudin ihsan <irfannudinihsan56@gmail.com>
2023-03-14 09:39:36 +0700

    category

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ const corsConfig = {
   credentials: true,
 };
 
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use(cors(corsConfig));
 app.use(function (req, res, next) {
